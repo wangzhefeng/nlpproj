9012e8309c982013879cb14718bf98aa25989627
wangzhefeng <wangzhefengr@163.com>
2025-07-24 16:46:57 +0800

    update tf codes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # nlpproj
+
+## Flow
+
+* 数据语言(data language)
+* 分词(tokenization)
+
+## Task
+
+* 命名实体识别(entity recognition)
+* 词性标注(part-of-speech-tagging)
+* 依存句法分析(dependency parsing)
+* 句子分割(sentence segmentation)
+* 文本分类(text classification)
+* 词元化(lemmatization)
+* 形态分析(morphological analysis)
+* 实体链接(entity linking)
+* ... 
+
+## Tools
+
+### spaCy
+
+* [spaCy](https://github.com/explosion/spaCy)
+* [spaCy models](https://github.com/explosion/spacy-models)
+* [NeuralCoref](https://github.com/huggingface/neuralcoref)
+* [textacy: NLP, before and after spaCy](https://textacy.readthedocs.io/en/stable/index.html)
+
+### Gensim
+
---
+++ scripts/update_codes.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# 运行方式：
+# 仅 pull: bash scripts/update_codes.sh pull
+# 要 push: bash scripts/update_codes.sh push
+
+echo "--------------------------"
+echo "update TinyLLMFinetuning codes..."
+echo "--------------------------"
+git checkout main
+echo "Successfully checked out main."
+
+if [ $1 == "push" ]; then
+    git add .
+    git commit -m "update tf codes"
+fi
+
+git pull
+echo "Successfully pulled the latest changes."
+
+if [ $1 == "push" ]; then
+    git push
+    echo "Successfully checked out master and updated the code."
+fi
+
+# push tokenizers
+# echo "--------------------------"
+# echo "update tokenizers codes..."
+# echo "--------------------------"
+# cd layers/tokenizers
+
+# if [ $1 == "push" ]; then
+#     git add .
+#     git commit -m "update"
+# fi
+
+# git pull
+# echo "Successfully pulled the latest changes."
+
+# if [ $1 == "push" ]; then
+#     git push
+#     echo "Successfully checked out master and updated the code."
+# fi
+
+# push utils
+echo "--------------------------"
+echo "update utils codes..."
+echo "--------------------------"
+cd utils
+
+if [ $1 == "push" ]; then
+    git add .
+    git commit -m "update"
+fi
+
+git pull
+echo "Successfully pulled the latest changes."
+
+if [ $1 == "push" ]; then
+    git push
+    echo "Successfully checked out master and updated the code."
+fi
