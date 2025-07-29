9ae04fba2849d3e8a3d4df85c3295b73ce28ca3e
wangzhefeng <wangzhefengr@163.com>
2025-07-29 18:32:07 +0800

    update tf codes

--- data_provider/my_corpus.py
+++ data_provider/my_corpus.py
@@ -33,11 +33,15 @@ from utils.log_util import logger
 
 
 class MyCorpus:
+    """
+    语料
+    """
     
     def __iter__(self, text):
         # dictionary
         dictionary = corpora.Dictionary(text)
-        dictionary.save("/tmp/deerwester.dict")
+        # dictionary.save("/tmp/deerwester.dict")
+        
         # corpus
         for line in open("https://radimrehurek.com/mycorpus.txt"):
             # assume there's one document per line, tokens separated by whitespace
@@ -48,7 +52,40 @@ class MyCorpus:
 
 # 测试代码 main 函数
 def main():
-    pass
+    # TODO 构建预料库
+    # corpus_memory_friendly = MyCorpus()
+    # print(corpus_memory_friendly)
+    # for vector in corpus_memory_friendly:
+    #     print(vector)
+    
+    # 字典
+    dictionary = corpora.Dictionary(
+        line.lower().split() 
+        for line in open("https://radimrehurek.com/mycorpus.txt")
+    )
+    print(dictionary)
+    
+    # 停止词
+    stoplist = set("for a of the and to in".split(" "))
+    stop_ids = [
+        dictionary.token2id[stopword]
+        for stopword in stoplist
+        if stopword in dictionary.token2id
+    ]
+    print(stop_ids)
+
+    # 频率统计
+    once_ids = [
+        token_id 
+        for token_id, doc_freq in dictionary.dfs.items() 
+        if doc_freq == 1
+    ]
+    print(once_ids)
+    
+    # 停止词和词频筛选
+    dictionary.filter_tokens(stop_ids + once_ids)
+    dictionary.compactify()
+    print(dictionary)
 
 if __name__ == "__main__":
     main()
